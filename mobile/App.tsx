940d74825fd54c00786e9a68ed851f8bfee187ff
Annehelen-ltda <71146646+Annehelen-ltda@users.noreply.github.com>
2020-12-08 18:37:32 -0300

    Revert "cadastro"
    This reverts commit 09c25c95b7c1cbfb19a180f6b0968da64449d026.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,13 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
-import Rotas from './src/routes';
 
 import { defaultTheme } from "./src/themes/defaultTheme";
 
 export default function App() {
 	return (
-		<Rotas />
+		<PaperProvider theme={defaultTheme}>
+			<View style={styles.container}>
+				<Text>Open up App.tsx to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		</PaperProvider>
 	);
 }
 
